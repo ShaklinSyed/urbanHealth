b77a23112e3ded0676d427b32f0013f8dc31dd59
Shaklin Syed <shaklin.mohammad@wellthy.care>
2023-07-14 11:03:08 +0530

    Dual Y axis complete

--- src/app/components/full-cont/full-cont.component.ts
+++ src/app/components/full-cont/full-cont.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, Renderer2, ViewChild } from '@angular/core';
+import { UtilService } from 'src/app/services/util.service';
+
+declare var google: any;
 
 @Component({
   selector: 'app-full-cont',
@@ -6,6 +9,46 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./full-cont.component.scss']
 })
 
-export class FullContComponent {
+export class FullContComponent implements AfterViewInit{
   @Input() data: any;
+  @ViewChild('chartContainer') chartRef: any;
+
+  elementId: String = '';
+
+  constructor(
+    private utilService: UtilService,
+    private renderer: Renderer2){
+
+  }
+
+  ngAfterViewInit(): void {
+    this.createIds();
+    this.renderCharts();
+  }
+
+  createIds(){
+    this.elementId = this.utilService.generateString(8);
+    this.renderer.setProperty(this.chartRef.nativeElement, "id", this.elementId);
+  }
+
+  renderCharts(){
+    google.charts.load('current', {'packages':['bar']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    let scope = this;
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable(scope.data.chartData);
+
+      var options = {
+        legend: 'bottom',
+        series: {
+          0: { axis: 'distance' }, // Bind series 0 to an axis named 'distance'.
+          1: { axis: 'brightness' } // Bind series 1 to an axis named 'brightness'.
+        }
+      };
+
+      var materialChart = new google.charts.Bar(document.getElementById(String(scope.elementId)));
+      materialChart.draw(data, google.charts.Bar.convertOptions(options));
+    }
+  }
 }
